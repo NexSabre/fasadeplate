9b1f40a3dc4d1bcf4df9c4d5a1ab133f2d5627c0
Nex Sabre <NexSabre@users.noreply.github.com>
2022-02-25 20:20:04 +0100

    .

---
+++ src/pages/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="row items-center">
+    <div class="items-center">
+      <h2>Diagonal</h2>
+      <br />
+
+      <q-numeric v-model="t" />
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex',
+  data() {
+    return {
+      t: 0,
+    };
+  },
+});
+</script>
